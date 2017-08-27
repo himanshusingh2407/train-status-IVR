341dea9b2ffd676206f84fc6f65e9aadad961c30
Himanshu Singh <himanshusingh2407@gmail.com>
2017-08-28 01:51:58 +0530

    Update response

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ var port = process.env.PORT || 5000
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(xml({response: [{
-      playaudio: 'https://himanshusingh2407.github.io/IVR/despacito.mp4'}, {
-      playtext: 'Hey i am Himanshu'
+      playtext: 'Hey i am Himanshu testing this api'
     }]}));
-
-    // response.set('Content-Type', 'text/xml');
-    // res.send(xml('<response><playaudio>https://himanshusingh2407.github.io/IVR/despacito.mp4</playaudio><playtext>Hey i am Himanshu</playtext><hangup/></response>'));
-
 }).listen(port);
 
 console.log(port);
