4978d46cce6757f1f483b1ef0e1f762cf595f1fa
suvam <suvam@motorola.com>
2017-08-31 00:38:35 +0530

    Reading response

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ var app = express();
 
 app.get('/', function(req, res){
   console.log(req.query);
+  var res_json = req.query;
+  var data = res_json['data'];
   res.header('Content-Type', 'text/xml');
-  res.send(xml({response: [{
-	collectdtmf: [ { _attr: { l: "5", t: "#"} }, 
-	{ playtext: 'Enter the 5 digit train number followed by Hash.'
+  if(data) {
+	res.send(xml({response: [{
+		playtext: 'You have entered ' + data
+    },
+	{
+		hangup: ''
 	}
-]}]}));
+    ]}));
+  } 
+  else {
+	  res.send(xml({response: [{
+		  collectdtmf: [ {
+			  _attr: { l: "5", t: "#"} 
+			}, 
+			{ 
+			  playtext: 'Enter the 5 digit train number followed by Hash.'
+			}
+		]}]}));
+  }
 });
 console.log('The app is running at PORT : ' + port);
 app.listen(port);
