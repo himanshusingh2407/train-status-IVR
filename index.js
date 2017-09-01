44f91936b8e1744535150230a3d3fac9582d709e
Himanshu Singh <himanshusingh2407@gmail.com>
2017-09-02 01:17:01 +0530

    Check response

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ var express = require('express');
 var xml = require('xml');
 var port = process.env.PORT || 5000
 var app = express();
+var trainApi = require('./util/train.js');
+
+// console.log(trainApi.getTrainStatus(15274, 1));
 
 app.get('/', function(req, res){
   console.log(req.query);
   res.header('Content-Type', 'text/xml');
   res.send(xml({response: [{
-    playtext: 'Hey welcome to our app. Just enter the 6 digit train number to know its running status.'
+    playtext: trainApi.getTrainStatus(15274, 1)
   }]}));
 });
 console.log('The app is running at PORT : ' + port);
