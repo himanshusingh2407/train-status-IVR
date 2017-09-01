47d3e896080b563644541f83af56090a369fe09a
Himanshu Singh <himanshusingh2407@gmail.com>
2017-09-02 01:06:13 +0530

    Add env variable

--- util/api.js
+++ util/api.js
@@ -1,10 +1,10 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
+//https://api.railbeeps.com/api
 module.exports = {
   //Calls the api using GET method and returns the JSON response
   callApi: function(url) {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url, false);
+    xhttp.open("GET", process.env.api + url, false);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
     return JSON.parse(xhttp.responseText);
--- util/train.js
+++ util/train.js
@@ -2,7 +2,7 @@ var date = require('./date.js');
 var api = require('./api.js');
 
 function getTrainID(trainNumber) {
-  var response = api.callApi("https://api.railbeeps.com/api/searchTrains?trainno=" + trainNumber);
+  var response = api.callApi("/searchTrains?trainno=" + trainNumber);
   return response[0].id;
 }
 
@@ -10,7 +10,7 @@ module.exports = {
   getTrainStatus: function(trainNumber, day) {
     var trainID = getTrainID(trainNumber);
     console.log(date.getDate(0));
-    var response = api.callApi("https://api.railbeeps.com/api/getRunningStatus/trainno/" + trainID + "/date/" + date.getDate(day));
+    var response = api.callApi("/getRunningStatus/trainno/" + trainID + "/date/" + date.getDate(day));
     let currentStation = response.markers.filter(station => station.status === 'current')[0].title;
     let stationCode = currentStation.split('(')[1].slice(0, -1);
     let delay = response.stations.filter(station => station.source_code === stationCode)[0].delay_arrival;
