e67578417e42f7ecc393914dab8a101a3bc88eba
Suvam Pramanik <pramaniksuvam@yahoo.com>
2017-09-04 02:00:18 +0530

    Modifying the kookoo functional flow. (#6)
    * First commit
    
    * First commit
    
    * First commit
    
    * First commit
    
    * First commit
    
    * First commit
    
    * Checking data inconsistency in kookoo.js
    
    * Checking data inconsistency in kookoo.js
    
    * Checking data inconsistency in kookoo.js
    
    * Modifying train data
    
    * Time zone issues
    
    * Time zone issues
    
    * Add select day option
    
    * Handling the edge cases
    
    * update msg
    
    * Handling the edge cases
    
    * Including say-as
    
    * Handling the edge cases
    
    * fix dtmf
    
    * Fix variable message error
    
    * Including journey date flow
    
    * Including journey date flow
    
    * Including journey date flow
    
    * Including journey date flow
    
    * Including journey date flow
    
    * Including journey date flow
    
    * Including journey date flow
    
    * Including journey date flow
    
    * Modifying the code flow
    
    * Removing unwanted comments
    
    * Removing unwanted comments

--- util/train.js
+++ util/train.js
@@ -1,19 +1,53 @@
 var date = require('./date.js');
 var api = require('./api.js');
 
-function getTrainID(trainNumber) {
-  var response = api.callApi("/searchTrains?trainno=" + trainNumber);
-  return response[0].id;
-}
-
 module.exports = {
-  getTrainStatus: function(trainNumber, day) {
-    var trainID = getTrainID(trainNumber);
-    console.log(date.getDate(0));
-    var response = api.callApi("/getRunningStatus/trainno/" + trainID + "/date/" + date.getDate(day));
-    let currentStation = response.markers.filter(station => station.status === 'current')[0].title;
-    let stationCode = currentStation.split('(')[1].slice(0, -1);
-    let delay = response.stations.filter(station => station.source_code === stationCode)[0].delay_arrival;
-    return ('The current station is ' + currentStation.split('(')[0] + ' and delay is ' + delay + ' minutes');
+  getTrainID: function(trainNumber) {
+    var response = api.callApi("/searchTrains?trainno=" + trainNumber);
+    if(response.length != 0)
+    return response[0].id;
+    else
+    return null;
+  },
+  getTrainStatus: function(trainID, day) {
+    var msg;
+    if(trainID) {
+      var response = api.callApi("/getRunningStatus/trainno/" + trainID + "/date/" + date.getDate(day));
+      if (response.errorMsg) {
+        return response.errorMsg;
+      }
+      let currentStation = response.markers.filter(station => station.status === 'current');
+      let stationCode, stationDisplayed, delay;
+      let trainName = response.details.name;
+      let trainNo = response.details.number;
+      console.log(trainNo);
+      let alertMessage = response.train.alertMsg;
+      if(currentStation.length != 0) {
+        stationDisplayed = currentStation[0].title.split('(')[0];
+        stationCode = currentStation[0].title.split('(')[1].slice(0, -1);
+        delay = response.stations.filter(station => station.source_code === stationCode)[0].delay_arrival;
+        console.log(stationCode);
+      }
+      else {
+        stationDisplayed = response.stations[response.stations.length-1].source_name;
+        delay = response.stations[response.stations.length-1].delay_arrival;
+      }
+      if(delay < 0 && Math.abs(delay)>=60) {
+        delay = 24*60 - Math.abs(delay);
+      }
+      if(delay <= 0){
+        msg = 'Train ' +  trainName + '        is currently at ' + stationDisplayed + ' station and is right on time.';
+      } else if(delay < 60) {
+        msg = 'Train ' +  trainName + '        is currently at ' + stationDisplayed + ' station and is late by ' + delay + ' minutes';
+      } else {
+        msg = 'Train ' +  trainName + '        is currently at ' + stationDisplayed + ' station and is late by ' + parseInt(delay/60) + ' hours and ' + delay%60 + ' minutes';
+      }
+      if(alertMessage)
+        msg = alertMessage;
+    }
+    else {
+      msg = 'Sorry, no train found by this train number. Please try again.';
+    }
+    return msg;
   }
 };
